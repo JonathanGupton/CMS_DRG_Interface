298e67355740f1e23e8982d66e5d5424eebfdcf0
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-08-10 10:18:28 -0500

    Add Batch class with tests

--- src/batch.py
+++ src/batch.py
@@ -1,4 +1,25 @@
+from typing import Iterable, Iterator, Optional
+
+from src.record import Record
 
 
 class Batch:
-    pass
+    """Object containing records for the CMS Batch grouper"""
+
+    def __init__(self, records: Optional[Iterable[Record]]) -> None:
+        if not records:
+            self.records = []
+        else:
+            self.records = list(records)
+
+    def __iter__(self) -> Iterator[Record]:
+        return iter(self.records)
+
+    def __len__(self) -> int:
+        return len(self.records)
+
+    def __str__(self) -> str:
+        return "".join(map(str, self))
+
+    def add_record(self, record) -> None:
+        self.records.append(record)
